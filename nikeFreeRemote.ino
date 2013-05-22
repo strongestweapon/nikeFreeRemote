f49d972e96a62984a61f730ee4590c93f1890f93
ossicode <artist@opensat.cc>
2013-05-22 17:19:50 +0900

    chan 2 button 2 functions

--- nikeFreeRemote.ino
+++ nikeFreeRemote.ino
@@ -9,6 +9,10 @@ int chan3State = 0;
 String inputString = "";
 boolean stringComplete = false;
 char inByte = 0;
+unsigned long pressedTime;
+unsigned long time;
+boolean chan2Pressed = false;
+boolean chan2Released = false;
 
 void setup()
 {
@@ -30,6 +34,7 @@ void loop()
      Serial.println("ok");
    }
   }
+  
   if(digitalRead(chan1Pin))
   {
     Serial.println("1");
@@ -38,9 +43,41 @@ void loop()
   
     if(digitalRead(chan2Pin))
   {
-    Serial.println("2");
-    delay(1000);
+    if (chan2Pressed == false)
+    {
+      chan2Pressed = true;
+      pressedTime = millis();
+    }
+    else
+    {
+      time = millis();
+      if ((time - pressedTime) > 1500)
+      {
+        Serial.println("0");
+        chan2Pressed = false;
+        delay(1000);
+      }
+    }
+
+    
   }
+  else
+  {
+
+    if (chan2Pressed == true)
+    {
+      time = millis();
+       if ((time - pressedTime) > 500)
+      {
+        Serial.println("2");
+        chan2Pressed = false;
+        delay(1000);
+      }
+    }
+    
+  }
+  
+  
   
     if(digitalRead(chan3Pin))
   {
